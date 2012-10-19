4ed71d369b3d18b13268cfb3d052d170fa9c23f9
Piyawit Piasakul <piyawit@artellectual.com>
2012-10-19 17:39:36 +0700

    add slug to category

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121019085905) do
+ActiveRecord::Schema.define(:version => 20121019101548) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(:version => 20121019085905) do
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "slug"
   end
 
   create_table "products", :force => true do |t|
