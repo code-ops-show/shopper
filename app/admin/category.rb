342419ba5f1f4168a73269340ecf8a4aecac4292
Piyawit Piasakul <piyawit@artellectual.com>
2012-10-19 17:41:35 +0700

    create category to activeadmin

---
+++ app/admin/category.rb
@@ -0,0 +1,28 @@
+ActiveAdmin.register Category do
+
+  action_item do
+    link_to "Add product", new_admin_category_path(category) if action_name == 'show'
+  end
+  
+  index do
+    column :name
+    column :description
+    default_actions
+  end
+
+  show title: :name do 
+    attributes_table do 
+      row :name
+      row :description
+    end
+
+    panel "Product" do 
+      table_for category.products do 
+        column :name
+        column :description
+      end
+    end
+  end
+
+  form partial: 'form'
+end
