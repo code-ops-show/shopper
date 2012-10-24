9a6b96b69d4faa50bb19aca9eca41fa3f9ea71d8
Piyawit Piasakul <piyawit@artellectual.com>
2012-10-24 18:00:34 +0700

    [Admin] customize view on product page

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -1,3 +1,15 @@
 ActiveAdmin.register Product do
   belongs_to :category
+
+  show title: :name do 
+    attributes_table do 
+      row :name
+      row :description
+      row :quantity
+      row :price
+      row "Cover" do |course|
+        image_tag(category.cover, { size: '300x200', crop: 'fill' })
+      end
+    end
+  end
 end
