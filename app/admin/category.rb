40bb096f21379f7c1acd73a03f47ff226e2774f5
Piyawit Piasakul <piyawit@artellectual.com>
2012-10-24 17:59:56 +0700

    add cover to this page

--- app/admin/category.rb
+++ app/admin/category.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Category do
   action_item do
-    link_to "Add product", new_admin_category_product_path(category) if action_name == 'show'
+    link_to "New product", new_admin_category_product_path(category) if action_name == 'show'
   end
   
   index do
@@ -13,12 +13,22 @@ ActiveAdmin.register Category do
     attributes_table do 
       row :name
       row :description
+      row "Cover" do |course|
+        image_tag(category.cover, { size: '150x100', crop: 'fill' })
+      end
     end
 
     panel "Product" do 
-      table_for category.products do 
-        column :name
+      table_for category.products do
+        column "Cover" do |product|
+          image_tag(product.cover, { size: '50x50', crop: 'fill' })
+        end
+        column "Name" do |product|
+          link_to product.name, admin_category_product_path(category, product)
+        end
         column :description
+        column :quantity
+        column :price
       end
     end
   end
