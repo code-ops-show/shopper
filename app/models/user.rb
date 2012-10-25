dccd2e257b2c864517cc6e4e105118840ec0147b
Piyawit Piasakul <piyawit@artellectual.com>
2012-10-25 10:59:36 +0700

    add imageUploader to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,13 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  mount_uploader :avatar, ImageUploader
+
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :avatar, :bio
   # attr_accessible :title, :body
+
+  def to_s
+    name or email
+  end
 end
