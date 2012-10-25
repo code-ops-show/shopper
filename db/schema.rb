20ae0f6abaf3aa0b95f1a409282cf8a7331a0d7f
Piyawit Piasakul <piyawit@artellectual.com>
2012-10-25 11:00:45 +0700

    add column avatar and bio to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121024090356) do
+ActiveRecord::Schema.define(:version => 20121025025327) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -80,6 +80,8 @@ ActiveRecord::Schema.define(:version => 20121024090356) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "name"
+    t.string   "avatar"
+    t.text     "bio"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
