319efb06abb61c9cb935db39866ad9bb2e583073
Piyawit Piasakul <piyawit@artellectual.com>
2012-11-20 09:52:55 +0700

    add validate to address

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,4 @@
 class Address < ActiveRecord::Base
-  
+  validates :street_address, :city, :state, :zip, :country, :phone, :email, :user_id, presence: true
+  attr_accessible :street_address, :city, :state, :zip, :country, :phone, :email, :user_id
 end
