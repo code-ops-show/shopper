7edc1e1781d609946f3141772385a0ba741eb698
Piyawit Piasakul <piyawit@artellectual.com>
2012-11-28 12:14:42 +0700

    update order purchase with addresses

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,19 @@ class OrdersController < ApplicationController
   def index
     @order ||= current_order
   end
+
+  def edit
+    @order = Order.find(params[:id])
+    @order.address = ( params[:order] and params[:order][:address_id].present? ) ? Address.find(params[:order][:address_id]) : Address.new
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    if @order.update_attributes(params[:order])
+      @order.purchase
+      redirect_to root_path
+    else
+      redirect_to :show
+    end
+  end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,8 @@
 class Address < ActiveRecord::Base
   belongs_to  :user
-  has_one     :order
+  has_many    :orders
 
   validates :street_address, :city, :state, :zip, :country, :phone, :email, :user_id, presence: true
   attr_accessible :street_address, :city, :state, :zip, :country, :phone, :email, :user_id
+  accepts_nested_attributes_for :orders
 end
