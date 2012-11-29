218bfcdc746854387e8647b0bb3f3fcd6948404f
Non Hasitabhan <non@artellectual.com>
2012-11-29 18:22:45 +0700

    updated view template

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,11 @@
 class Product < ActiveRecord::Base
   belongs_to :category
   has_many   :items
-  has_many   :orders,         through: :items
+  has_many   :orders, through: :items
 
   mount_uploader :cover, ImageUploader
 
   attr_accessible :category_id, :name, :description, :quantity, :price, :cover
-
 
   scope :last_four_products, order('created_at DESC').limit(4)
 
