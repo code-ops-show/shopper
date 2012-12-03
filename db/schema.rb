3ac102f9c8a2fe551ddd81d921109dbf8047b16c
Piyawit Piasakul <piyawit@artellectual.com>
2012-12-03 10:25:24 +0700

    add default field to address

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121128084403) do
+ActiveRecord::Schema.define(:version => 20121130035227) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(:version => 20121128084403) do
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
     t.integer  "user_id"
+    t.boolean  "default"
   end
 
   create_table "admin_users", :force => true do |t|
