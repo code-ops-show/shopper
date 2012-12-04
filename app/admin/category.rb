a063735685f0ecea667464d576a5d9d738b8726e
Non Hasitabhan <non@artellectual.com>
2012-12-04 09:14:31 +0700

    updated cart, checkout template

--- app/admin/category.rb
+++ app/admin/category.rb
@@ -14,19 +14,21 @@ ActiveAdmin.register Category do
       row :name
       row :description
       row "Cover" do |course|
-        image_tag(category.cover, { size: '150x100', crop: 'fill' })
+        image_tag(category.cover, { size: '150x100' })
       end
     end
 
     panel "Product" do 
       table_for category.products do
         column "Cover" do |product|
-          image_tag(product.cover, { size: '50x50', crop: 'fill' })
+          image_tag(product.cover, { size: '50x50' })
         end
         column "Name" do |product|
           link_to product.name, admin_category_product_path(category, product)
         end
-        column :description
+        column "Description" do |product| 
+          truncate(product.description, length: 400)
+        end
         column :quantity
         column :price
       end
