3ba30ba94d93bf74d9821498861cbfe3b117025f
Non Hasitabhan <non@artellectual.com>
2012-12-04 09:14:31 +0700

    updated cart, checkout template

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   def token
     cookies[:token] ? cookies[:token] : rand(2468**10).to_s(32)
   end
-
+  
   def current_order
     cookies[:token] = { value: token, expires: 1.hour.from_now }
     @current_order ||= Order.where(token: token, state: 'cart').first || Order.create!(token: token)
