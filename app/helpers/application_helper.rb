44eec4c71d81dde8b5d77ab299916c1b31f154d7
Non Hasitabhan <non@artellectual.com>
2012-12-04 09:14:31 +0700

    updated cart, checkout template

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def render_cart_menu
+    order = Order.where(token: cookies[:token], state: 'cart').first
+    
+    if current_user and request.path == edit_order_path(order)
+      link_to edit_order_path(order) do
+        "<i class=\"icon-shopping-cart icon-white\"></i> Cart".html_safe
+      end
+    else
+      link_to orders_path, remote: true do
+        "<i class=\"icon-shopping-cart icon-white\"></i> Cart".html_safe
+      end
+    end
+  end
 end
