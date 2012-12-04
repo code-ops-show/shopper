fcbca31aa12ae282624cb4687f65aecd39121fb8
Non Hasitabhan <non@artellectual.com>
2012-12-04 12:20:15 +0700

    cleanup navigation to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,54 @@
 module ApplicationHelper
+  def render_menu_for text, path
+    active = request.path == path ? 'active' : ''
+
+    content_tag :li, class: active do
+      link_to text, path
+    end
+  end
+
   def render_cart_menu
-    order = Order.where(token: cookies[:token], state: 'cart').first
+    active = request.path == edit_order_path(current_order.id) ? 'active' : ''
+    size = current_order.items.size 
     
-    if current_user and request.path == edit_order_path(order)
-      link_to edit_order_path(order) do
-        "<i class=\"icon-shopping-cart icon-white\"></i> Cart".html_safe
+    content_tag :li, class: active do
+      if current_user and request.path == edit_order_path(current_order.id)
+        link_to edit_order_path(current_order.id) do
+          "<i class=\"icon-shopping-cart icon-white\"></i> Cart (#{size})".html_safe
+        end
+      else
+        link_to orders_path, remote: true do
+          "<i class=\"icon-shopping-cart icon-white\"></i> Cart (#{size})".html_safe
+        end
       end
-    else
-      link_to orders_path, remote: true do
-        "<i class=\"icon-shopping-cart icon-white\"></i> Cart".html_safe
+    end
+  end
+
+  def render_user_menu
+    active = 'active' if request.path.match(/users/)
+
+    content_tag :li, class: "dropdown #{active}" do
+      if current_user
+        link = link_to '#', class: 'dropdown-toggle', data: { toggle: 'dropdown' } do
+          concat "#{current_user} <b class=\"caret\"></b>".html_safe
+        end
+        menu = content_tag :ul, class: "dropdown-menu" do
+          concat render_menu_for("My Profile", edit_user_registration_path)
+          concat render_menu_for("My Order", user_orders_path(current_user))
+          concat content_tag :li, (link_to "Log out", destroy_user_session_path, method: :delete)
+        end
+
+        link.concat menu
+      else
+        link = link_to '#', class: 'dropdown-toggle', data: { toggle: 'dropdown' } do
+          concat "Login / Register <b class=\"caret\"></b>".html_safe
+        end
+        menu = content_tag :ul, class: "dropdown-menu" do
+          concat render_menu_for("Login", new_user_session_path)
+          concat render_menu_for("Register", new_user_registration_path)
+        end
+
+        link.concat menu
       end
     end
   end
