a1605969b12f9a1e1791aaffd38069164156ec47
Piyawit Piasakul <piyawit@artellectual.com>
2012-12-07 19:19:03 +0700

    country belong to shipping rate

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,6 +1,6 @@
 class Country < ActiveRecord::Base
   has_many :cities
-  has_and_belongs_to_many :shipping_rates
+  belongs_to :shipping_rate
 
-  attr_accessible :name
+  attr_accessible :name, :shipping_rate_id
 end
