ed2ba2b4dd31bdbdcb282175fd1208f18ed6e9a9
Piyawit Piasakul <piyawit@artellectual.com>
2012-12-11 17:22:15 +0700

    update spec order controller

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -2,8 +2,26 @@ require 'spec_helper'
 
 describe OrdersController do
 
-  let(:order)     { Order.make!(token: cookies[:token]) }
-  let!(:address)   { order.address }
+  let(:order)       { Order.make! }
+  let!(:address)    { order.address }
+  let(:user)        { address.user }
+
+  describe "GET 'show" do
+   it "should return http success" do
+      get :show, id: order.id, format: :js
+      response.should be_success
+    end
+
+    it "should assign order not be nil" do
+      get :show, id: order.id, format: :js
+      assigns[:order].should_not be_nil
+    end
+
+    it "should assign order" do
+      get :show, id: order.id, format: :js
+      assigns[:order].should eq order
+    end
+  end
 
   describe "GET 'index'" do
     it "should return http success" do
@@ -27,21 +45,53 @@ describe OrdersController do
       get :edit, id: order.id, format: :js
       assigns[:order].should eq order
     end
+
+    it "should assign order address" do
+      get :edit, id: order.id, format: :js
+      assigns[:order].address.should_not be_nil
+    end
+
+    it "should assign order address" do
+      get :edit, id: order.id, format: :js
+      assigns[:order].address.id.should be_nil
+    end
+
+    it "should assign order address with default address should not nil" do
+      sign_in user
+      get :edit, id: order.id, format: :js
+      assigns[:order].address.should_not be_nil
+    end
+
+    it "should assign order address with default address" do
+      sign_in user
+      get :edit, id: order.id, format: :js
+      assigns[:order].address.should eq address
+    end
+
+    it "should assign order address with find by param should not nil" do
+      get :edit, id: order.id, order: order, format: :js
+      assigns[:order].address.should_not be_nil
+    end
+
+     it "should assign order address with find by param" do
+      get :edit, id: order.id, order: order, format: :js
+      assigns[:order].address.should eq address
+    end
   end
 
   describe "PUT 'update'" do
     it "should assign order" do
-      put :update, id: order.id
+      put :update, id: order.id, order: { address_id: address.id }
       assigns[:order].should_not be_nil
     end
 
     it "should assign order" do
-      put :update, id: order.id
+      put :update, id: order.id, order: { address_id: address.id }
       assigns[:order].should eq order
     end
 
-    it "should redirects back to show" do
-      put :update, id: order.id
+    it "should redirects back to root path" do
+      put :update, id: order.id, order: { address_id: address.id }
       response.should redirect_to root_path
     end
   end
