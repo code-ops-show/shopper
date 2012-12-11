0512bb29b68f95d85c3a80e3a1961d242f8bd695
Piyawit Piasakul <piyawit@artellectual.com>
2012-12-11 17:20:48 +0700

    spec user model update

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,10 +5,19 @@ describe User do
   it { should have_many(:orders).through(:addresses) }
 
   describe "Model setup and utilities" do
-    let(:user)   { User.make! }
+    let(:address)     { Address.make! }
+    let(:user)        { address.user }
+
+    it "should update with password" do
+      user.update_with_password.should be_true
+    end
 
     it "should to_s equal email" do
       user.to_s.should equal user.email
+    end
+
+    it "should return default address" do
+      user.default_address.should eq address
     end
   end
 end
