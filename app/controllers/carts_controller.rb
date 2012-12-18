3d7f2cdc98509e77db3dd283bfc6b0da65490840
Piyawit Piasakul <piyawit@artellectual.com>
2012-12-18 17:01:56 +0700

    check current user and update some condition to change state to purchase

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,12 +1,17 @@
 class CartsController < OrdersController
   def edit
     @cart = Order.where(id: params[:id]).includes(items: [:product]).first
-    @cart.address = current_user.select_address_for(params[:order]) || Address.new
+    @cart.address = current_or_guest_user.select_address_for(params[:order]) || Address.new
   end
 
   def update
     @cart = Order.find(params[:id])
-    @cart.purchase if @cart.update_attributes!(params[:order])
-    redirect_to root_path
+    if @cart.update_attributes(params[:order])
+      @cart.purchase if params[:order][:address_id] or (current_or_guest_user and params[:order][:address_attributes])
+      reset_session
+      redirect_to root_path
+    else 
+      render_box_error_for(@cart)
+    end
   end
 end
