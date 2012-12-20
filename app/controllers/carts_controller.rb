65d7603a76baebd981df080ecad69369ebe7a428
Piyawit Piasakul <piyawit@artellectual.com>
2012-12-20 09:55:02 +0700

    return to root path after purchase

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,5 @@
 class CartsController < OrdersController
+
   def edit
     @cart = Order.where(id: params[:id]).includes(items: [:product]).first
     @cart.address = @cart.address || (current_or_guest_user and current_or_guest_user.default_address) || Address.new
@@ -7,16 +8,18 @@ class CartsController < OrdersController
   def update
     @cart = Order.find(params[:id])
     if @cart.update_attributes(params[:order])
-      session.delete(:guest_user_id)
+      respond_to do |format|
+        format.js {
+          render action: "update_items" if params[:order][:items_attributes]
+          render action: "update_addresses" if params[:order][:address_id] or params[:order][:addresses_attributes]
+        }
+
+        format.html {
+          redirect_to root_path
+        }
+      end
     else 
       render_box_error_for(@cart)
-    end
-
-    respond_to do |format|
-      format.js {
-        render action: "update_items" if params[:order][:items_attributes]
-        render action: "update_addresses" if params[:order][:address_id] or params[:order][:addresses_attributes]
-      }
     end
   end
 end
