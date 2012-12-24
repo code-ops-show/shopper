9281711a4d9d5303c0e9f5bf38f014c2fddcc80b
Piyawit Piasakul <piyawit@artellectual.com>
2012-12-24 12:40:13 +0700

    delete unused

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -7,13 +7,5 @@ class AdminUser < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, 
-         :recoverable, :rememberable, :trackable, :validatable
-
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
 end
