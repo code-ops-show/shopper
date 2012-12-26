46b43e38b43d96929aeec149088b49dba2d7d8af
Non Hasitabhan <non@artellectual.com>
2012-12-26 12:48:39 +0700

    updated guest create account move to guests_controller

--- config/routes.rb
+++ config/routes.rb
@@ -10,18 +10,23 @@ Shopper::Application.routes.draw do
       resources :addresses, only: [:index]
     end
   end
+  
   resources :categories, only: [:index] do
     resources :products, only: [:index, :show]
   end
+  
   resources :products do 
     resources :items, only: [:create]
   end
+  
   resources :orders do 
     get ":id/status/:status", action: :show, as: :status, on: :collection
   end
+  
   resources :carts do
     resources :addresses, only: [:new, :create]
   end
+  
   resources :items
   resources :addresses
   resources :users do
@@ -29,7 +34,7 @@ Shopper::Application.routes.draw do
     resources :addresses
   end
   
+  resources :guests, only: [:new, :create, :update]
+  
   root to: 'home#index'
-
-  resources :guests, only: [:new, :create]
 end
