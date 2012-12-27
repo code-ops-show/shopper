187769406ae968f6fa8536b2a87233b08948b71a
Piyawit Piasakul <piyawit@artellectual.com>
2012-12-27 13:20:17 +0700

    change purchase state name in mailer

--- spec/mailers/order_mailer_spec.rb
+++ spec/mailers/order_mailer_spec.rb
@@ -2,10 +2,10 @@ require "spec_helper"
 
 describe OrderMailer do
   describe "purchased_state," do
-    let(:mail) { OrderMailer.purchased_state, }
+    let(:mail) { OrderMailer.purchased_state }
 
     it "renders the headers" do
-      mail.subject.should eq("Purchased state,")
+      mail.subject.should eq("Purchased state")
       mail.to.should eq(["to@example.org"])
       mail.from.should eq(["from@example.com"])
     end
