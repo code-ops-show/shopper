34039bf2400b6ce3735f471c2d27d43d915cfa21
Non Hasitabhan <non@artellectual.com>
2013-01-08 15:38:34 +0700

    added products requests spec

--- spec/requests/products_requests_spec.rb
+++ spec/requests/products_requests_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
 describe "Products Requests" do
-  let(:member)    { Member.make! }
+  let(:member) { User.make!(:member) }
+  let(:order)  { Order.make! }
 
   before :all do
     @product = Product.make!(name: "Product 001", price: 100)
     @product2 = Product.make!(name: "Product 002", price: 200)
+    @product3 = Product.make!(name: "Product 003", price: 300, quantity: 0)
     @category = @product.category
     @category2 = @product2.category
   end
@@ -25,7 +27,18 @@ describe "Products Requests" do
       page.should have_content "Product 002"
     end
 
-    it "should add product to cart" do
+    it "should add product to cart", js: true do
+      within "#product_#{@product.id}" do
+        click_button "Add Cart"
+      end
+      page.should have_content "Cart (1)"
+    end
+
+    it "should show out of stock when available" do
+      within "#product_#{@product3.id}" do
+        page.should have_content "Out of Stock"
+        page.should_not have_content "Add to Cart"
+      end
     end
 
     context "filter" do
@@ -38,6 +51,7 @@ describe "Products Requests" do
 
         page.should have_content "Product 001"
         page.should_not have_content "Product 002"
+        page.should_not have_content "Product 003"
       end
 
       it "should search product by keyword" do
@@ -46,6 +60,7 @@ describe "Products Requests" do
 
         page.should have_content "Product 001"
         page.should_not have_content "Product 002"
+        page.should_not have_content "Product 003"
       end
     end
 
@@ -54,13 +69,13 @@ describe "Products Requests" do
         select "Name: A-Z", from: 'sort_by'
 
         within "ul.thumbnails li:first-child" do page.should have_content "Product 001" end
-        within "ul.thumbnails li:last-child"  do page.should have_content "Product 002" end
+        within "ul.thumbnails li:last-child"  do page.should have_content "Product 003" end
       end
 
       it "should sorting by name Z-A" do
         select "Name: Z-A", from: 'sort_by'
 
-        within "ul.thumbnails li:first-child" do page.should have_content "Product 002" end
+        within "ul.thumbnails li:first-child" do page.should have_content "Product 003" end
         within "ul.thumbnails li:last-child"  do page.should have_content "Product 001" end
       end
 
@@ -68,23 +83,35 @@ describe "Products Requests" do
         select "Price: Low to High", from: 'sort_by'
 
         within "ul.thumbnails li:first-child" do page.should have_content "Product 001" end
-        within "ul.thumbnails li:last-child"  do page.should have_content "Product 002" end
+        within "ul.thumbnails li:last-child"  do page.should have_content "Product 003" end
       end
 
       it "should sorting by Price: High to Low" do
         select "Price: High to Low", from: 'sort_by'
 
-        within "ul.thumbnails li:first-child" do page.should have_content "Product 002" end
+        within "ul.thumbnails li:first-child" do page.should have_content "Product 003" end
         within "ul.thumbnails li:last-child"  do page.should have_content "Product 001" end
       end
     end
   end
 
   context "#show" do
-    it "should add product to cart" do
+    it "should add product to cart", js: true do
+      within "#product_#{@product.id}" do
+        click_link "Product 001"
+      end
+
+      click_button "Add Cart"
+      page.should have_content "Cart (1)"
     end
 
     it "should show out of stock when unavailable" do
+      within "#product_#{@product3.id}" do
+        click_link "Product 003"
+      end
+
+      page.should have_content "Out of Stock"
+      page.should_not have_content "Add to Cart"
     end
   end
 end
